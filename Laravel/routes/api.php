7016599695adbc54016e3a9b22702178a407e130
AbuSufiunDev <abusufiun.dev@gmail.com>
2024-04-25 14:36:25 +0600

    can not config the axios

--- Laravel/routes/api.php
+++ Laravel/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,6 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/students', [StudentController::class, 'index'])->name('studenst');
 Route::post('/students', [StudentController::class, 'store'])->name('store');
+
+Route::post('/signup', [AuthController::class, 'signup'])->name('signup');
+
